1c5dfe47b899591c934246f3362eb57549348e28
Robby <ta7382@gmail.com>
2018-09-08 17:33:53 +0800

    [refactor] i18n

---
+++ src/i18n/en.js
@@ -0,0 +1,15 @@
+const en_US = { // eslint-disable-line
+  'intl.lang': 'Language',
+  'intl.lang.en': 'English',
+  'intl.lang.zh-tw': 'Traditional Chinese',
+  'intl.lang.zh-cn': 'Simplified Chinese',
+  'intl.hello': 'Hello!',
+  'intl.name': 'My name is {name}',
+  'intl.title': `Full-Stack's Front-End Engineer`,
+  'intl.edu': 'B. B. A. ,NUTC',
+  'intl.intro': `Hi, I'm <b>Robby</b>.<br>
+    I'm from Taichung, Taiwan, and I'm a research student.<br>
+    I love to write blog about Web Development.<br><br>
+    Nice to meet you !`,
+}
+export default en_US // eslint-disable-line
---
+++ src/i18n/zh-cn.js
@@ -0,0 +1,12 @@
+const zh_CN = { // eslint-disable-line
+  'intl.lang': '语系',
+  'intl.lang.en': '英文',
+  'intl.lang.zh-tw': '繁体中文',
+  'intl.lang.zh-cn': '简体中文',
+  'intl.hello': '你好!',
+  'intl.name': '我的名字是 {name}',
+  'intl.title': '全端的 Front-End Engineer',
+  'intl.edu': '國立臺中科技大學 資訊管理系',
+  'intl.intro': `你好，我是 <b>Robby</b>.<br>我來自臺灣的臺中，目前是研究生。<br>喜歡研究技術，也有經營 Blog，把踩過的坑記錄成文章。<br>本網站為個人簡歷介紹，記錄作品分享、技術心得等資訊。<br><br>很高興認識你！`,
+}
+export default zh_CN // eslint-disable-line
