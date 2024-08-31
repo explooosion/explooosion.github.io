6404c9bc2399c38dd6c4f5e36c76897cb3022ae3
Robby Wu <ta7382@gmail.com>
2024-08-31 18:25:14 +0800

    Update translation.json for English and Chinese locales

--- web/src/containers/Header/Icons.tsx
+++ web/src/containers/Header/Icons.tsx
@@ -1,13 +1,14 @@
-import { memo } from 'react';
-
+import { memo } from "react";
 import {
-  FaSquareGithub, FaSquareGitlab,
+  FaSquareGithub,
+  FaSquareGitlab,
   FaLinkedin,
   FaSquareRss,
-  FaCodepen, FaInbox
+  FaCodepen,
+  FaInbox,
+  FaSquareYoutube,
 } from "react-icons/fa6";
-
-import { IconType } from 'react-icons/lib';
+import { IconType } from "react-icons/lib";
 
 interface IResource {
   key: string;
@@ -31,19 +32,40 @@ const Icon = memo(function Icon({ resource }: { resource: IResource }) {
 
 const Icons = memo(function Icons() {
   const resources: IResource[] = [
-    { key: 'github', link: 'https://github.com/explooosion', img: FaSquareGithub },
-    { key: 'gitlab', link: 'https://gitlab.com/explooosion', img: FaSquareGitlab },
-    { key: 'blog', link: 'https://dotblogs.com.tw/explooosion', img: FaSquareRss },
-    { key: 'linkedin', link: 'https://www.linkedin.com/in/robby-wu-2b26b510a', img: FaLinkedin },
-    { key: 'codepen', link: 'https://codepen.io/ta7382', img: FaCodepen },
-    { key: 'email', link: 'mailto:ta7382@gmail.com', img: FaInbox },
+    {
+      key: "github",
+      link: "https://github.com/explooosion",
+      img: FaSquareGithub,
+    },
+    {
+      key: "gitlab",
+      link: "https://gitlab.com/explooosion",
+      img: FaSquareGitlab,
+    },
+    {
+      key: "blog",
+      link: "https://dotblogs.com.tw/explooosion",
+      img: FaSquareRss,
+    },
+    {
+      key: "linkedin",
+      link: "https://www.linkedin.com/in/robby-wu-2b26b510a",
+      img: FaLinkedin,
+    },
+    { key: "codepen", link: "https://codepen.io/ta7382", img: FaCodepen },
+    {
+      key: "youtube",
+      link: "https://www.youtube.com/@robbywu570",
+      img: FaSquareYoutube,
+    },
+    { key: "email", link: "mailto:ta7382@gmail.com", img: FaInbox },
   ];
 
   return (
     <div className="flex items-center justify-between w-4/5 max-w-[250px]">
-      {resources.map(
-        resource => <Icon key={resource.key} resource={resource} />
-      )}
+      {resources.map((resource) => (
+        <Icon key={resource.key} resource={resource} />
+      ))}
     </div>
   );
 });
